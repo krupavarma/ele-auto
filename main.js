776d4121685f50e37cba5d6a45984abe7846d35a
kvarma@jazeeratechnologies.com <kvarma@jazeeratechnologies.com>
2019-12-09 11:36:36 +0530

    commit v.20

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // This is free and unencumbered software released into the public domain.
 // See LICENSE for details
 
-const { app, BrowserWindow, Menu, protocol, ipcMain } = require("electron");
-const log = require("electron-log");
-const { autoUpdater } = require("electron-updater");
+const { app, BrowserWindow, Menu, protocol, ipcMain } = require('electron');
+const log = require('electron-log');
+const { autoUpdater } = require('electron-updater');
 
 //-------------------------------------------------------------------
 // Logging
@@ -14,8 +14,8 @@ const { autoUpdater } = require("electron-updater");
 // but it sure makes debugging easier :)
 //-------------------------------------------------------------------
 autoUpdater.logger = log;
-autoUpdater.logger.transports.file.level = "info";
-log.info("App starting...");
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
 
 //-------------------------------------------------------------------
 // Define the menu
@@ -23,19 +23,19 @@ log.info("App starting...");
 // THIS SECTION IS NOT REQUIRED
 //-------------------------------------------------------------------
 let template = [];
-if (process.platform === "darwin") {
+if (process.platform === 'darwin') {
   // OS X
   const name = app.getName();
   template.unshift({
     label: name,
     submenu: [
       {
-        label: "About " + name,
-        role: "about"
+        label: 'About ' + name,
+        role: 'about'
       },
       {
-        label: "Quit",
-        accelerator: "Command+Q",
+        label: 'Quit',
+        accelerator: 'Command+Q',
         click() {
           app.quit();
         }
@@ -57,7 +57,7 @@ let win;
 
 function sendStatusToWindow(text) {
   log.info(text);
-  win.webContents.send("message", text);
+  win.webContents.send('message', text);
 }
 function createDefaultWindow() {
   win = new BrowserWindow({
@@ -69,7 +69,7 @@ function createDefaultWindow() {
     height: 720
   });
   win.webContents.openDevTools();
-  win.on("closed", () => {
+  win.on('closed', () => {
     win = null;
   });
   // win.loadURL('file://' + __dirname + '/version.html');
@@ -78,53 +78,54 @@ function createDefaultWindow() {
   return win;
 }
 autoUpdater.setFeedURL({
-  provider: "github",
-  owner: "krupavarma",
-  url: "com.github.krupavarma.ele-auto",
-  token: Sys.getenv("GH_TOKEN")
+  provider: 'github',
+  owner: 'krupavarma',
+  url: 'https://github.com/krupavarma/ele-auto.git',
+  // token: process.env('GH_TOKEN')
+  token: process.env.NODE_ENV
 });
-autoUpdater.on("checking-for-update", () => {
-  sendStatusToWindow("Checking for update...");
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
 });
-autoUpdater.on("update-available", info => {
-  sendStatusToWindow("Update available.");
+autoUpdater.on('update-available', info => {
+  sendStatusToWindow('Update available.');
 });
-autoUpdater.on("update-not-available", info => {
-  sendStatusToWindow("Update not available.");
+autoUpdater.on('update-not-available', info => {
+  sendStatusToWindow('Update not available.');
 });
-autoUpdater.on("error", err => {
-  sendStatusToWindow("Error in auto-updater. " + err);
+autoUpdater.on('error', err => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
 });
-autoUpdater.on("download-progress", progressObj => {
-  let log_message = "Download speed: " + progressObj.bytesPerSecond;
-  log_message = log_message + " - Downloaded " + progressObj.percent + "%";
+autoUpdater.on('download-progress', progressObj => {
+  let log_message = 'Download speed: ' + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
   log_message =
     log_message +
-    " (" +
+    ' (' +
     progressObj.transferred +
-    "/" +
+    '/' +
     progressObj.total +
-    ")";
+    ')';
   sendStatusToWindow(log_message);
 });
-autoUpdater.on("update-downloaded", (ev, info) => {
+autoUpdater.on('update-downloaded', (ev, info) => {
   // Wait 5 seconds, then quit and install
   // In your application, you don't need to wait 5 seconds.
   // You could call autoUpdater.quitAndInstall(); immediately
-  sendStatusToWindow("Update downloaded");
+  sendStatusToWindow('Update downloaded');
 
   setTimeout(function() {
     autoUpdater.quitAndInstall();
   }, 5000);
 });
-app.on("ready", function() {
+app.on('ready', function() {
   // Create the Menu
   const menu = Menu.buildFromTemplate(template);
   Menu.setApplicationMenu(menu);
 
   createDefaultWindow();
 });
-app.on("window-all-closed", () => {
+app.on('window-all-closed', () => {
   app.quit();
 });
 
@@ -138,7 +139,7 @@ app.on("window-all-closed", () => {
 // This will immediately download an update, then install when the
 // app quits.
 //-------------------------------------------------------------------
-app.on("ready", function() {
+app.on('ready', function() {
   autoUpdater.checkForUpdatesAndNotify();
 });
 
